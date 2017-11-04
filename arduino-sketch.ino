329f85ef8823919b7982e06452edd14ab52ebb6f
Manik <manik15csu109@ncuindia.edu>
2017-11-04 15:21:05 +0530

    Arduino Sketch changes

--- arduino-sketch.ino
+++ arduino-sketch.ino
@@ -1,8 +1,6 @@
 #include <dht.h>
 dht DHT;
 int sensorValue;
-// if you require to change the pin number, Edit the pin with your arduino pin.
-
 #define DHT11_PIN 5
 
 void setup() 
@@ -15,20 +13,6 @@ void loop()
   int chk = DHT.read11(DHT11_PIN);
   String data;
   sensorValue = analogRead(0); 
-
-  /*Serial.println(" Humidity " );
-
-  Serial.println(DHT.humidity);
-  delay(200);
-
-  Serial.println(" Temparature ");
-
-  Serial.println(DHT.temperature);
-  delay(200);
-
-  Serial.println(" Air Quality ");
-
-  Serial.println(sensorValue);*/
   data= String(DHT.humidity)+' '+String(DHT.temperature)+' '+String(sensorValue);
   Serial.println(data);
   delay(1000);
